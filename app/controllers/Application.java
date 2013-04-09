82c28f114228076762a8801809526c1f7d6dbd30
Helder Jaspio <helder.jaspion@gmail.com>
2013-04-09 10:20:46 -0300

    upload de imagens

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,40 +1,128 @@
 package controllers;
 
 import static play.data.Form.form;
+
+import java.io.File;
+import java.sql.Blob;
+import java.util.List;
+
+import models.Picture;
+
+import org.codehaus.jackson.node.ArrayNode;
+import org.codehaus.jackson.node.JsonNodeFactory;
+import org.codehaus.jackson.node.ObjectNode;
+
+import play.Logger;
 import play.data.Form;
+import play.libs.Json;
+import play.mvc.BodyParser;
 import play.mvc.Controller;
+import play.mvc.Http.MultipartFormData;
+import play.mvc.Http.MultipartFormData.FilePart;
 import play.mvc.Result;
 import views.html.login;
+import views.html.upload;
 
 public class Application extends Controller {
-	
+
 	public static Result login() {
 		return ok(login.render(form(Login.class)));
 	}
-	
+
 	public static Result logout() {
-		
+
 		session().clear();
-		
+
 		flash("success", "Você foi desconectado.");
-		
-        return redirect(
-            routes.Application.login()
-        );
+
+		return redirect(routes.Application.login());
 	}
-	
+
 	public static Result autenticar() {
 		Form<Login> loginForm = form(Login.class).bindFromRequest();
-		
-		if(!loginForm.hasErrors()) {
+
+		if (!loginForm.hasErrors()) {
 			session("email", loginForm.get().email);
 			return redirect(routes.Contas.index());
 		} else {
 			return badRequest(login.render(loginForm));
 		}
 	}
+
+	public static Result startUpload() {
+		return ok(upload.render());
+	}
 	
-	
-	
+	public static Result getPicture(long id) {
+		Picture picture = Picture.find.byId(id);
+		return ok("");
+//		response().setContentType(picture.image)
+//		Ok(picture.image.getBinaryStream().
+	}
+
+	public static Result upload() {
+		ObjectNode result = Json.newObject();
+		ArrayNode array = result.putArray("files");
+
+		// PrintWriter writer = response.getWriter();
+		// response.setContentType("application/json");
+		// JSONArray json = new JSONArray();
+		// try {
+		// List<FileItem> items = uploadHandler.parseRequest(request);
+		// for (FileItem item : items) {
+		// if (!item.isFormField()) {
+		// File file = new File(request.getServletContext().getRealPath("/")+"imgs/", item.getName());
+		// item.write(file);
+		// JSONObject jsono = new JSONObject();
+		// jsono.put("name", item.getName());
+		// jsono.put("size", item.getSize());
+		// jsono.put("url", "UploadServlet?getfile=" + item.getName());
+		// jsono.put("thumbnail_url", "UploadServlet?getthumb=" + item.getName());
+		// jsono.put("delete_url", "UploadServlet?delfile=" + item.getName());
+		// jsono.put("delete_type", "GET");
+		// json.put(jsono);
+		// System.out.println(json.toString());
+		// }
+		// }
+		// } catch (FileUploadException e) {
+		// throw new RuntimeException(e);
+		// } catch (Exception e) {
+		// throw new RuntimeException(e);
+		// } finally {
+		// writer.write(json.toString());
+		// writer.close();
+		// }
+
+		MultipartFormData body = request().body().asMultipartFormData();
+		
+		List<FilePart> files = body.getFiles();
+		for(FilePart filePart : files) {
+			ObjectNode fileJson = new ObjectNode(JsonNodeFactory.instance);
+			fileJson.put("name", filePart.getFilename());
+			fileJson.put("size", filePart.getFile().length());
+			try {
+				fileJson.put("url", filePart.getFile().getCanonicalPath());
+			} catch (Exception e) { Logger.debug("", e);}
+//			fileJson.put("thumbnail_url", filePart.getFile().getName());
+//			fileJson.put("delete_url", filePart.getFile().getName());
+//			fileJson.put("delete_type", "DELETE");
+			Logger.info(filePart.toString());
+			array.add(fileJson);
+		}
+		
+		return ok(result);
+		
+//		FilePart picture = body.getFile("picture");
+//		if (picture != null) {
+//			String fileName = picture.getFilename();
+//			String contentType = picture.getContentType();
+//			File file = picture.getFile();
+//			Logger.info(file.getAbsolutePath());
+//			return ok(result);
+//		} else {
+//			flash("error", "Missing file");
+//			return redirect(routes.Application.login());
+//		}
+	}
 
 }
--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -11,6 +11,25 @@ create table conta (
   constraint pk_conta primary key (id))
 ;
 
+create table homenageado (
+  id                        bigint not null,
+  nome                      varchar(255),
+  constraint pk_homenageado primary key (id))
+;
+
+create table picture (
+  id                        bigint not null,
+  url                       varchar(255),
+  constraint pk_picture primary key (id))
+;
+
+create table tipo_homenagem (
+  id                        bigint not null,
+  descricao                 varchar(255),
+  sub_tipo_homenagem_id     bigint,
+  constraint pk_tipo_homenagem primary key (id))
+;
+
 create table usuario (
   id                        bigint not null,
   email                     varchar(255),
@@ -19,14 +38,32 @@ create table usuario (
   constraint pk_usuario primary key (id))
 ;
 
+
+create table usuario_homenageado (
+  usuario_id                     bigint not null,
+  homenageado_id                 bigint not null,
+  constraint pk_usuario_homenageado primary key (usuario_id, homenageado_id))
+;
 create sequence conta_seq;
+
+create sequence homenageado_seq;
+
+create sequence picture_seq;
+
+create sequence tipo_homenagem_seq;
 
 create sequence usuario_seq;
 
 alter table conta add constraint fk_conta_usuario_1 foreign key (usuario_id) references usuario (id) on delete restrict on update restrict;
 create index ix_conta_usuario_1 on conta (usuario_id);
+alter table tipo_homenagem add constraint fk_tipo_homenagem_subTipoHomen_2 foreign key (sub_tipo_homenagem_id) references tipo_homenagem (id) on delete restrict on update restrict;
+create index ix_tipo_homenagem_subTipoHomen_2 on tipo_homenagem (sub_tipo_homenagem_id);
 
 
+
+alter table usuario_homenageado add constraint fk_usuario_homenageado_usuari_01 foreign key (usuario_id) references usuario (id) on delete restrict on update restrict;
+
+alter table usuario_homenageado add constraint fk_usuario_homenageado_homena_02 foreign key (homenageado_id) references homenageado (id) on delete restrict on update restrict;
 
 # --- !Downs
 
@@ -34,11 +71,25 @@ SET REFERENTIAL_INTEGRITY FALSE;
 
 drop table if exists conta;
 
+drop table if exists homenageado;
+
+drop table if exists picture;
+
+drop table if exists tipo_homenagem;
+
 drop table if exists usuario;
+
+drop table if exists usuario_homenageado;
 
 SET REFERENTIAL_INTEGRITY TRUE;
 
 drop sequence if exists conta_seq;
+
+drop sequence if exists homenageado_seq;
+
+drop sequence if exists picture_seq;
+
+drop sequence if exists tipo_homenagem_seq;
 
 drop sequence if exists usuario_seq;
 
