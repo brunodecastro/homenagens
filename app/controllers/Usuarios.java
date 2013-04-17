b4a53b4efc3be93eaceb211d659a0abe8e136883
Bruno <brunnodecastro@gmail.com>
2013-04-17 08:57:50 -0300

    atualizado

--- app/controllers/Usuarios.java
+++ app/controllers/Usuarios.java
@@ -22,59 +22,65 @@ public class Usuarios extends Controller {
 	public static Result index() {
 		return GO_HOME;
 	}
-	
-	/**
-     * Display the 'new computer form'.
-     */
-    public static Result criar() {
-    	Form<Usuario> usuarioForm = form(Usuario.class);
-        return ok(
-            criarUsuario.render(usuarioForm, Usuario.consultarPorEmail(request().username()))
-        );
-    }
-    
-    /**
-     * Display the 'edit form' of a existing Computer.
-     *
-     * @param id Id of the computer to edit
-     */
-    public static Result editar(Long id) {
-    	return ok();
-    }
-    
-    /**
-     * Handle the 'new computer form' submission 
-     */
-    public static Result salvar() {
-        Form<Usuario> contaForm = form(Usuario.class).bindFromRequest();
-        
-        if(contaForm.hasErrors()) {
-            return badRequest(criarUsuario.render(contaForm, Usuario.consultarPorEmail(request().username())));
-        }
-        
-        contaForm.get().save();
-        flash("success", "Usuario " + contaForm.get().nome + " foi criado com sucesso.");
-        return GO_HOME;
-    }
-	
-	/**
-     * Display the paginated list of usuarios.
-     *
-     * @param page Current page number (starts from 0)
-     * @param sortBy Column to be sorted
-     * @param order Sort order (either asc or desc)
-     * @param filter Filter applied on conta names
-     */
-    public static Result listar(int page, String sortBy, String order, String filter) {
 
-    	Usuario usuario = Usuario.consultarPorEmail(request().username());
-    	Page<Usuario> usuarioPage = Usuario.listPage(page, 10, sortBy, order, filter);
+	/**
+	 * Display the 'new computer form'.
+	 */
+	public static Result criar() {
+		Form<Usuario> usuarioForm = form(Usuario.class);
+		return ok(criarUsuario.render(usuarioForm, Usuario.consultarPorEmail(request().username())));
+	}
 
-    	return ok(
-            list.render(usuario, usuarioPage, sortBy, order, filter
-            )
-        );
-    }
+	/**
+	 * Display the 'edit form' of a existing Computer.
+	 * 
+	 * @param id
+	 *            Id of the computer to edit
+	 */
+	public static Result editar(Long id) {
+		return ok();
+	}
+
+	/**
+	 * Handle the 'new computer form' submission
+	 */
+	public static Result salvar() {
+		Form<Usuario> usuarioForm = form(Usuario.class).bindFromRequest();
+
+		// Check repeated password
+		if (!usuarioForm.field("senha").valueOr("").isEmpty()) {
+			if (!usuarioForm.field("senha").valueOr("").equals(usuarioForm.field("confirmacaoSenha").value())) {
+				usuarioForm.reject("confirmacaoSenha", "Senha não confere.");
+			}
+		}
+
+		if (usuarioForm.hasErrors()) {
+			return badRequest(criarUsuario.render(usuarioForm, Usuario.consultarPorEmail(request().username())));
+		}
+
+		usuarioForm.get().save();
+		flash("success", "Usuario " + usuarioForm.get().nome + " foi criado com sucesso.");
+		return GO_HOME;
+	}
+
+	/**
+	 * Display the paginated list of usuarios.
+	 * 
+	 * @param page
+	 *            Current page number (starts from 0)
+	 * @param sortBy
+	 *            Column to be sorted
+	 * @param order
+	 *            Sort order (either asc or desc)
+	 * @param filter
+	 *            Filter applied on conta names
+	 */
+	public static Result listar(int page, String sortBy, String order, String filter) {
+
+		Usuario usuario = Usuario.consultarPorEmail(request().username());
+		Page<Usuario> usuarioPage = Usuario.listPage(page, 10, sortBy, order, filter);
+
+		return ok(list.render(usuario, usuarioPage, sortBy, order, filter));
+	}
 
 }
-
