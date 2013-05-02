a0cd790ce0e88f6a78c393f4b93027ebc6402e32
Bruno <brunnodecastro@gmail.com>
2013-05-02 16:14:24 -0300

    df

--- app/controllers/Usuarios.java
+++ app/controllers/Usuarios.java
@@ -1,16 +1,13 @@
 package controllers;
 
 import static play.data.Form.form;
-import models.TipoHomenagem;
 import models.Usuario;
 import play.data.Form;
 import play.i18n.Messages;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
-import views.html.tipoHomenagem.editForm;
-import views.html.usuario.criarUsuario;
-import views.html.usuario.list;
+import views.html.usuario.*;
 
 import com.avaje.ebean.Page;
 
@@ -20,7 +17,7 @@ public class Usuarios extends Controller {
 	/**
 	 * This result directly redirect to application home.
 	 */
-	public static Result GO_HOME = redirect(routes.Usuarios.listar(0, "nome", "asc", ""));
+	public static Result GO_HOME = redirect(routes.Usuarios.list(0, "nome", "asc", ""));
 
 	public static Result index() {
 		return GO_HOME;
@@ -29,9 +26,9 @@ public class Usuarios extends Controller {
 	/**
 	 * Display the 'new computer form'.
 	 */
-	public static Result criar() {
+	public static Result create() {
 		Form<Usuario> usuarioForm = form(Usuario.class);
-		return ok(criarUsuario.render(usuarioForm, Usuario.consultarPorEmail(request().username())));
+		return ok(createForm.render(usuarioForm, Usuario.consultarPorEmail(request().username())));
 	}
 
 	/**
@@ -40,21 +37,64 @@ public class Usuarios extends Controller {
 	 * @param id
 	 *            Id of the computer to edit
 	 */
-	public static Result editar(Long id) {
-//		Usuario usuario = Usuario.find.byId(id);
-//        Form<Usuario> usuarioForm = form(Usuario.class).fill(
-//        		usuario
-//        );
-        return ok(
-//            editForm.render(Usuario.consultarPorEmail(request().username()), id, usuarioForm)
-        );
+	public static Result edit(Long id) {
+		Usuario usuario = Usuario.find.byId(id);
+		Form<Usuario> usuarioForm = form(Usuario.class).fill(usuario);
+		return ok(editForm.render(Usuario.consultarPorEmail(request().username()), id, usuarioForm));
+	}
+
+	/**
+	 * Handle the 'edit form' submission
+	 * 
+	 * @param id
+	 *            Id of the usuario to edit
+	 */
+	public static Result update(Long id) {
+		Form<Usuario> usuarioForm = form(Usuario.class).bindFromRequest();
+		
+		//Validate the form
+		validateForm(usuarioForm);
+		
+		if (usuarioForm.hasErrors()) {
+			return badRequest(editForm.render(Usuario.consultarPorEmail(request().username()), id, usuarioForm));
+		}
+		
+		usuarioForm.get().update(id);
+		
+		flash("success", "O Usuário \"" + usuarioForm.get().nome + "\" foi atualizado.");
+		return GO_HOME;
+	}
+
+	/**
+	 * Handle usuario deletion
+	 */
+	public static Result delete(Long id) {
+		Usuario usuario = Usuario.find.ref(id);
+		String deletedName = usuario.nome;
+		usuario.delete();
+		flash("success", "O Usuário \"" + deletedName + "\" foi excluído.");
+		return GO_HOME;
 	}
 
 	/**
 	 * Handle the 'new computer form' submission
 	 */
-	public static Result salvar() {
+	public static Result save() {
 		Form<Usuario> usuarioForm = form(Usuario.class).bindFromRequest();
+
+		//Validate the form
+		validateForm(usuarioForm);
+
+		if (usuarioForm.hasErrors()) {
+			return badRequest(createForm.render(usuarioForm, Usuario.consultarPorEmail(request().username())));
+		}
+
+		usuarioForm.get().save();
+		flash("success", Messages.get("usuario.create.success", usuarioForm.get().nome));
+		return GO_HOME;
+	}
+
+	private static void validateForm(Form<Usuario> usuarioForm) {
 
 		// Check repeated password
 		if (!usuarioForm.field("senha").valueOr("").isEmpty()) {
@@ -63,13 +103,6 @@ public class Usuarios extends Controller {
 			}
 		}
 
-		if (usuarioForm.hasErrors()) {
-			return badRequest(criarUsuario.render(usuarioForm, Usuario.consultarPorEmail(request().username())));
-		}
-
-		usuarioForm.get().save();
-		flash("success", Messages.get("usuario.create.success", usuarioForm.get().nome));
-		return GO_HOME;
 	}
 
 	/**
@@ -84,7 +117,7 @@ public class Usuarios extends Controller {
 	 * @param filter
 	 *            Filter applied on conta names
 	 */
-	public static Result listar(int page, String sortBy, String order, String filter) {
+	public static Result list(int page, String sortBy, String order, String filter) {
 
 		Usuario usuario = Usuario.consultarPorEmail(request().username());
 		Page<Usuario> usuarioPage = Usuario.listPage(page, 10, sortBy, order, filter);
