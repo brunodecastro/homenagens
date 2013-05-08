2977487a53119e3230ed4facbd81b78e4e517837
Bruno <brunnodecastro@gmail.com>
2013-05-08 19:04:51 -0300

    correçoes

--- app/models/Homenagem.java
+++ app/models/Homenagem.java
@@ -3,9 +3,13 @@ package models;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.validation.Valid;
 
+import play.data.validation.Constraints.Required;
 import play.db.ebean.Model;
 
 public class Homenagem extends Model {
@@ -13,8 +17,14 @@ public class Homenagem extends Model {
 	@Id
 	public Long id;
 	
-	public Homenagem homenagem;
+	@ManyToOne(fetch=FetchType.LAZY)
+	public Homenagem homenagemPai;
 	
+	@Valid
+	@OneToMany(mappedBy="homenagemPai")
+	public List<Homenagem> homenagensFilhas;
+	
+	@Required
 	public String numeroRegistro;
 	
 	public Homenageado homenageado;
