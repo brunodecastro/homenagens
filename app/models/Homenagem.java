bedab34d355fea88d5ada1a0961f290dc7dc7293
Bruno <brunnodecastro@gmail.com>
2013-06-04 19:53:02 -0300

    .

--- app/models/Homenagem.java
+++ app/models/Homenagem.java
@@ -34,10 +34,12 @@ public class Homenagem extends Model {
 	@Required
 	public String numeroRegistro;
 	
+	@Required
 	public Homenageado homenageado;
 	
 	public String descricao;
 
+	@Required
 	@OneToOne
 	public TipoHomenagem tipoHomenagem;
 	
