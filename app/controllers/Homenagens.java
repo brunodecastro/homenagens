b24d6ecd2e3a0b3335fbc11ed7331ec052999415
Helder Jaspion <helder.jaspion@gmail.com>
2013-07-02 18:02:22 -0300

    fixing fileupload

--- app/controllers/Homenagens.java
+++ app/controllers/Homenagens.java
@@ -2,23 +2,37 @@ package controllers;
 
 import static play.data.Form.form;
 
+import java.io.File;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import org.apache.commons.io.FileUtils;
+import org.codehaus.jackson.node.ArrayNode;
+import org.codehaus.jackson.node.ObjectNode;
 
 import models.Cidade;
 import models.Estado;
 import models.Homenageado;
 import models.Homenagem;
 import models.HomenagemFilter;
+import models.HomenagemImagem;
+import models.RawImage;
 import models.Usuario;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import play.Configuration;
+import play.Logger;
+import play.Play;
 import play.data.Form;
 import play.i18n.Messages;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.mvc.Results;
 import play.mvc.Security;
+import play.mvc.Http.MultipartFormData;
+import play.mvc.Http.MultipartFormData.FilePart;
 import views.html.homenagem.createForm;
 import views.html.homenagem.editForm;
 import views.html.homenagem.list;
@@ -172,5 +186,95 @@ public class Homenagens extends Controller {
         return ReportController.jasperDocument("homenagem_list", reportParams, new
     			JRBeanCollectionDataSource(homenagemList));
     }
+    
+    
+    public static Result getAllImages(Long homenagemId) {
+		ObjectNode result = Json.newObject();
+		ArrayNode array = result.putArray("files");
+		
+		Homenagem homenagem = Homenagem.find.byId(homenagemId);
+
+		List<HomenagemImagem> bdImages = homenagem.imagens;
+		for (HomenagemImagem image : bdImages) {
+			array.add(dbImageToJson(image));
+		}
+
+		return Results.ok(result);
+	}
+    
+    /**
+	 * @return json with all infos of the newly created or updated images
+	 */
+	public static Result uploadImage(Long homenagemId) {
+		ObjectNode result = Json.newObject();
+		ArrayNode array = result.putArray("files");
+
+		try {
+			MultipartFormData body = Controller.request().body().asMultipartFormData();
+
+			List<FilePart> fileParts = body.getFiles();
+
+			for (FilePart filePart : fileParts) {
+				HomenagemImagem image = new HomenagemImagem();
+				image.homenagem = Homenagem.find.byId(homenagemId);
+				// image.setId(id);
+				image.setFilename(filePart.getFilename());
+				File imgFile = filePart.getFile();
+				byte[] bytes = FileUtils.readFileToByteArray(imgFile);
+				setRawImage(image, bytes);
+				if (!isAllowedMimetype(image)) {
+					return Results.status(400, "mimetype not allowed");
+				}
+				setRawThumbnail(image);
+				image.save();
+				array.add(dbImageToJson(image));
+			}
+
+			return Results.ok(result);
+		} catch (Exception e) {
+			Logger.warn(e.getMessage(), e);
+			return Results.status(400, e.getMessage());
+		}
+	}
+	
+	private static boolean isAllowedMimetype(HomenagemImagem image) {
+		Configuration config = Play.application().configuration();
+		int index = Arrays.binarySearch(config.getString("image.mimetypes.allowed").split(","), image.getImage().getMimetype());
+		return index >= 0;
+	}
+
+	private static void setRawImage(HomenagemImagem image, byte[] bytes) {
+		RawImage rawImage = image.getImage();
+		if (rawImage == null) {
+			rawImage = new RawImage();
+			image.setImage(rawImage);
+		}
+		rawImage.setImage(bytes);
+	}
+
+	private static void setRawThumbnail(HomenagemImagem image) {
+		Configuration config = Play.application().configuration();
+		int thumbnailWidth = config.getInt("thumbnails.width");
+		int thumbnailHeight = config.getInt("thumbnails.height");
+		RawImage thumbnail = image.getThumbnail();
+		if (thumbnail == null) {
+			thumbnail = new RawImage();
+			image.setThumbnail(thumbnail);
+		}
+		byte[] thumbnailBytes = image.getImage().createThumbnail(thumbnailWidth, thumbnailHeight);
+		thumbnail.setImage(thumbnailBytes);
+	}
+    
+    private static ObjectNode dbImageToJson(HomenagemImagem image) {
+		ObjectNode result = Json.newObject();
+		result.put("id", image.getId());
+		result.put("name", image.getFilename());
+		result.put("size", image.getImage().getImage().length);
+		result.put("url", "/image/" + image.getId());
+		result.put("thumbnail_url", "/thumbnail/" + image.getId());
+		result.put("delete_url", "/image/" + image.getId());
+		result.put("delete_type", "DELETE");
+		return result;
+	}
     
 }
